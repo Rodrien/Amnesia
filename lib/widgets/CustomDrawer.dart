25e66d80337c84408b42b97bd8b1ebb305ec53c2
Rodrien <enrodriguez1998@gmail.com>
2021-04-29 23:02:35 -0300

    drawerOptionsAdded

--- lib/widgets/CustomDrawer.dart
+++ lib/widgets/CustomDrawer.dart
@@ -14,41 +14,129 @@ class CustomDrawer extends StatelessWidget {
             Container(
               color: systemInf.colorFondoPrincipal,
               height: 150,
+              child: Text("aux"),
             ),
             Expanded(
               child: Container(
                 //aca un listview cion opciones
                 color: systemInf.colorCajonPasswords,
+                child: ListView(
+                  padding: EdgeInsets.all(10),
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 0,
+                        bottom: 5,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.create_rounded,
+                            color: Colors.white,
+                          ),
+                          tileColor: systemInf.colorFondoPrincipal,
+                          title: Text(
+                            "Generador de contraseñas",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {},
+                          selectedTileColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.import_export_rounded,
+                            color: Colors.white,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          tileColor: systemInf.colorFondoPrincipal,
+                          title: Text(
+                            "Importar/Exportar",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {},
+                          selectedTileColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.settings,
+                            color: Colors.white,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          tileColor: systemInf.colorFondoPrincipal,
+                          title: Text(
+                            "Configuracion",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {},
+                          selectedTileColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.contact_support_rounded,
+                            color: Colors.white,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          tileColor: systemInf.colorFondoPrincipal,
+                          title: Text(
+                            "Contacto",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onTap: () {},
+                          selectedTileColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Container(
-              color: systemInf.colorFondoPrincipal,
-              height: 20,
-              child: RichText(
-                text: TextSpan(
-                  children: <TextSpan>[
-                    TextSpan(
-                      text: "Creado por",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        letterSpacing: 1,
-                      ),
-                    ),
-                    TextSpan(
-                      text: " Kike Dev",
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: systemInf.colorImportante,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        letterSpacing: 1,
-                      ),
-                    )
-                  ],
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.only(left: 0),
+              padding: EdgeInsets.only(
+                top: 0,
+                bottom: 0,
+              ),
+              child: IconButton(
+                icon: Icon(
+                  Icons.lightbulb_outline_rounded,
+                  color: Colors.white,
                 ),
+                onPressed: () {}, //open bottom drawer to change theme
               ),
             ),
           ],
